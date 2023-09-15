045e6c906df5e4e79382ce33f97097942d4474d6
unknown <jhowjhoe@icloud.com>
2023-09-15 00:30:47 -0300

    upload and docs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import { mg } from './scrap/mg';
 
+dotenv.config();
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
