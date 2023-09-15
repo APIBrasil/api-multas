5da8ecd0b42b1416cf55c2fe833483109a7129d6
unknown <jhowjhoe@icloud.com>
2023-09-15 00:45:26 -0300

    fix env and server

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ app.post('/multas/mg', async (req, res) => {
 
 })
 
-app.listen(2222, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
+})
 
